2dc12cf815f79751902bac40723d39b24060dfd9
Matt Walker <matt.walker@genesis.global>
2023-11-03 14:35:35 +0000

    produce fixed manfiest

--- packages/web-components/fast-components/custom-elements-manifest.config.js
+++ packages/web-components/fast-components/custom-elements-manifest.config.js
@@ -1,17 +1,25 @@
 import { jsdocLinkFix } from "./custom-elements-manifest-plugins.js";
+import importAliasPlugin from "@genesiscommunitysuccess/analyzer-import-alias-plugin";
 
 export default {
-	/** Globs to analyze */
-	globs: ["src/**/*.{ts,js}"],
-	/** Directory to output CEM to */
-	outdir: "./dist",
-	/** Run in dev mode, provides extra logging */
-	dev: false,
-	/** Enable special handling for FastElement */
-	fast: true,
-	/** Include third party custom elements manifests */
-	dependencies: true,
-	/** Output CEM path to `package.json`, defaults to true */
-	packagejson: true,
-	plugins: [jsdocLinkFix()],
+    /** Globs to analyze */
+    globs: ["src/**/*.{ts,js}"],
+    /** Directory to output CEM to */
+    outdir: "./dist",
+    /** Run in dev mode, provides extra logging */
+    dev: false,
+    /** Enable special handling for FastElement */
+    fast: true,
+    /** Include third party custom elements manifests */
+    dependencies: true,
+    /** Output CEM path to `package.json`, defaults to true */
+    packagejson: true,
+    plugins: [
+        jsdocLinkFix(),
+        importAliasPlugin({
+            "@microsoft/fast-foundation": {
+                "*": name => name.replace("Foundation", ""),
+            },
+        }),
+    ],
 };
